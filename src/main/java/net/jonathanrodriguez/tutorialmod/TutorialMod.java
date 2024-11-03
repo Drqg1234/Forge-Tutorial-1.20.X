d2331a8f9892b7930a2ecd99b1db7e05419c668b
Drqg1234 <jonathan.rodriguez9921@gmail.com>
2024-11-02 22:04:29 -0500

    Important changes made

--- src/main/java/net/jonathanrodriguez/tutorialmod/TutorialMod.java
+++ src/main/java/net/jonathanrodriguez/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.jonathanrodriguez.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.jonathanrodriguez.tutorialmod.item.ModCreativeModTabs;
+import net.jonathanrodriguez.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,6 +30,8 @@ public class TutorialMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -42,9 +47,14 @@ public class TutorialMod
 
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
+    // Adds items to pre-existing minecraft tabs i.e., creative tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        // Adds sapphire to creative mode tab as an ingredient
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
