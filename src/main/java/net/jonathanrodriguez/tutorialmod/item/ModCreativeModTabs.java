5777624992c2c234f99c590155ae6a5a2a96bc09
Drqg1234 <jonathan.rodriguez9921@gmail.com>
2024-11-13 02:07:48 -0600

    DataGen (pain)

--- src/main/java/net/jonathanrodriguez/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/jonathanrodriguez/tutorialmod/item/ModCreativeModTabs.java
@@ -35,6 +35,17 @@ public class ModCreativeModTabs {
                         output.accept(ModItems.STRAWBERRY.get());
                         output.accept(ModItems.PINE_CONE.get());
 
+                        output.accept(ModBlocks.SAPPHIRE_SLAB.get());
+                        output.accept(ModBlocks.SAPPHIRE_STAIRS.get());
+                        output.accept(ModBlocks.SAPPHIRE_WALL.get());
+                        output.accept(ModBlocks.SAPPHIRE_TRAPDOOR.get());
+                        output.accept(ModBlocks.SAPPHIRE_FENCE.get());
+                        output.accept(ModBlocks.SAPPHIRE_FENCE_GATE.get());
+                        output.accept(ModBlocks.SAPPHIRE_DOOR.get());
+                        output.accept(ModBlocks.SAPPHIRE_BUTTON.get());
+                        output.accept(ModBlocks.SAPPHIRE_PRESSURE_PLATE.get());
+
+
                     })
                     .build());
 
